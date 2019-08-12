3ac7a92c62d98d423ebaf0581d98793d94574476
aviatesk <aviatesk@gmail.com>
2019-08-12 16:52:25 +0900

    write up tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using APipe
 using Test
 
-function λ𝟎(keyword = "keyword")
+function λ𝟎(; keyword = "keyword")
     keyword
 end
 
@@ -9,10 +9,20 @@ function λ𝟏(arg; keyword = "keyword")
     arg, keyword
 end
 
+function λ(arg, default = "default"; keyword = "keyword")
+    arg, default, keyword
+end
+
 @testset "APipe" begin
     @testset "applied to function" begin
         @test ("chained", "keyword") == @> "chained" |> λ𝟏
         @test ("chained", "keyword") == @> "chained" |> (1, λ𝟏)
         @test ("chained") == @> "chained" |> (:keyword, λ𝟎)
+    end
+
+    @testset "applied to function call" begin
+        @test ("chained", "default", "keyword") == @> "chained" |> (1, λ())
+        @test ("passed", "chained", "keyword") == @> "chained" |> (2, λ("passed"))
+        @test ("passed", "default", "chained") == @> "chained" |> (:keyword, λ("passed"))
     end
 end
