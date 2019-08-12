815fdaf6737e2959432b35183b837234b09c9650
aviatesk <aviatesk@gmail.com>
2019-08-12 11:37:48 +0900

    basic test setup

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
-using FlexPipe
+using APipe
 using Test
 
-@testset "Test setup" begin
-    @test true
+function λ𝟎(keyword = "keyword")
+    keyword
+end
+
+function λ𝟏(arg; keyword = "keyword")
+    arg, keyword
+end
+
+@testset "APipe" begin
+    @testset "applied to function" begin
+        @test ("chained", "keyword") == @> "chained" |> λ𝟏
+        @test ("chained", "keyword") == @> "chained" |> (1, λ𝟏)
+        @test ("chained") == @> "chained" |> (:keyword, λ𝟎)
+    end
 end
