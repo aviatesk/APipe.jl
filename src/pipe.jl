7d08c60abcad9fdfd23c88a1226959b1601c634d
aviatesk <aviatesk@gmail.com>
2019-08-12 11:26:09 +0900

    basic implementations
    - cover every functionality of original `|>` operator

---
+++ src/pipe.jl
@@ -0,0 +1,41 @@
+function processexpr!(expr) end # fallback
+
+function processexpr!(expr::Expr)
+    head = expr.head
+    args = expr.args
+
+    if head === :call && args[1] === :|>
+        # process inside `|>` operators first
+        processexpr!(args[2])
+
+        if args[3] isa Expr && args[3].head === :tuple
+            tpl = args[3].args
+            pipearg, fcall = tpl
+
+            # verbose ?
+            @assert length(tpl) === 2 &&
+                    (pipearg isa Int || (pipearg isa QuoteNode && pipearg.value isa Symbol)) &&
+                    (fcall isa Symbol || (fcall isa Expr && fcall.head === :call))
+
+            # manipulate chanined expression
+            args[3] = if fcall isa Symbol
+                makechainfunc(fcall, pipearg)
+            else # `fcall` isa `Expr`
+                makechainfunc(fcall, pipearg)
+            end
+        end
+    end
+end
+
+function makechainfunc(fcall::Symbol, pipearg::Int)
+    # the other arguments are not specified and thus `pipearg` should be `1`
+    @assert pipearg === 1
+    :(piped -> $fcall(piped))
+end
+
+function makechainfunc(fcall::Symbol, pipearg::QuoteNode)
+    :(piped -> $fcall(; $(pipearg.value) = piped))
+end
+
+function makechainfunc(fcall::Expr, pipearg::Int) end
+function makechainfunc(fcall::Expr, pipearg::Symbol) end
